490415b83c11c902bc7b7e1128182d130829f92a
adammaran <adamrumunce@gmail.com>
2021-04-13 20:05:52 +0200

    fixed merge conflicts

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -2,20 +2,35 @@ import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 class AppUser extends Equatable {
-  const AppUser(
-      {@required this.email,
-      @required this.id,
-      @required this.emailVerified,
-      this.topics});
+  const AppUser({
+    @required this.email,
+    @required this.id,
+    @required this.emailVerified,
+    @required this.firstName,
+    @required this.lastName,
+    @required this.dateOfBirth,
+    @required this.gender,
+  });
 
   final String email;
   final String id;
   final bool emailVerified;
-  final List<String> topics;
+  final String firstName;
+  final String lastName;
+  final String dateOfBirth;
+  final String gender;
 
-  static const empty =
-      AppUser(email: '', id: '', emailVerified: false, topics: []);
+  static const empty = AppUser(
+    email: '',
+    id: '',
+    emailVerified: false,
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    gender: '',
+  );
 
   @override
-  List<Object> get props => [email, id, emailVerified];
+  List<Object> get props =>
+      [email, id, emailVerified, firstName, lastName, dateOfBirth, gender];
 }
